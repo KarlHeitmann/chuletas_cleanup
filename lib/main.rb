2bcf1aaf9de13dae304ce9ecd3138c6178a3d990
Karl Heitmann <khf1988@gmail.com>
2022-06-24 22:42:53 -0400

    some basic tests added
    two tests added
    
    tests are passing
    
    created functions getDiffFile and getIOData to atomize code
    
    ready one test for IO

--- lib/main.rb
+++ lib/main.rb
@@ -2,52 +2,58 @@
 
 OFFENSES = ["ERASE_ME", "HACK_ME", "XXX_ME", "binding.pry", "puts"]
 
-def mainDiff
-  #cmd1 = "git diff --cached --name-only --diff-filter=ACM -z | xargs -0 ...".split(" ")
+class IOUtils # HACK_ME
+  def dummy
+    "dummy foobar"
+  end
+  def getCmdData(cmd)
+    io = IO.popen(cmd)
+    data = io.read
+    io.close
+    data
+  end
+end
+
+def mainDiff(io)
   cmd1 = "git diff --cached --name-only --diff-filter=ACM".split(" ")
-  #cmd1 = "git status".split(" ")
-  io = IO.popen(cmd1)
-  files = io.read
-  io.close
-  files
+  io.getCmdData(cmd1)
 end
 
 def analyze(diffs)
-  puts diffs.inspect
-  offenses = []
+  offenses = {}
+  offenses.default_proc = proc { 0 }
   start_diff = if diffs[1].split(" ")[0] == "new"
                  6
                else
                  5
                end
   diffs = diffs[start_diff..]
-  # Up untill now, I have only changes in variable diffs.
   diffs.each do |diff|
     OFFENSES.each do |offense|
-      offenses << diff if diff.include? offense
+      offenses[offense] += 1 if diff.include? offense
     end
   end
-  return diffs
+  return offenses
+end
+
+def getDiffFile(io, file)
+  cmd = "git diff HEAD -U0 #{file}"
+  io.getCmdData(cmd).split("\n")
 end
 
 def run
-  listing = mainDiff
+  io = IOUtils.new
+  listing = mainDiff(io)
   raise "it failed!" unless $?.exitstatus == 0
   listing.split("\n").each do |file|
-    puts ":::: FILE #{file}"
     extension = file.split('.')[-1]
     next unless extension == 'rb'
-    cmd = "git diff HEAD -U0 #{file}"
-    io = IO.popen(cmd)
-    #diffs = io.read.split("\n")[4..]
-    #diffs = io.read.split("\n")[5..]
-    diffs = io.read.split("\n")
-    io.close
+    diffs = getDiffFile(io, file)
     results = analyze(diffs)
     puts results
   end
   exit 1
 end
 
-run
+# run
 
