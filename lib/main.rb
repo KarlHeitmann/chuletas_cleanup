7190419ee6ebc7b6c268acbc78f15f8328e4a28f
Karl Heitmann <khf1988@gmail.com>
2022-06-25 18:29:03 -0400

    passed some subocop

--- lib/main.rb
+++ lib/main.rb
@@ -1,39 +1,35 @@
 #!/usr/bin/ruby
+# frozen_string_literal: true
 
-OFFENSES = ["ERASE_ME", "HACK_ME", "XXX_ME", "binding.pry", "puts"]
+OFFENSES = ['ERASE_ME', 'HACK_ME', 'XXX_ME', 'binding.pry', 'puts'].freeze
 
+# IOUtils is used to retrieve data fro IO sucprocess with only one command HACK_ME
 class IOUtils
-  def dummy
-    "dummy foobar"
-  end
   def getCmdData(cmd)
     io = IO.popen(cmd)
     data = io.read
     io.close
+    # raise 'it failed!' unless $?.exitstatus == 0
     data
   end
 end
 
 def mainDiff(io)
-  cmd1 = "git diff --cached --name-only --diff-filter=ACM".split(" ")
+  cmd1 = 'git diff --cached --name-only --diff-filter=ACM'.split
   io.getCmdData(cmd1)
 end
 
 def analyze(diffs)
   offenses = {}
   offenses.default_proc = proc { 0 }
-  start_diff = if diffs[1].split(" ")[0] == "new"
-                 6
-               else
-                 5
-               end
+  start_diff = diffs[1].split[0] == 'new' ? 6 : 5
   diffs = diffs[start_diff..]
   diffs.each do |diff|
     OFFENSES.each do |offense|
       offenses[offense] += 1 if diff.include? offense
     end
   end
-  return offenses
+  offenses
 end
 
 def getDiffFile(io, file)
@@ -44,10 +40,11 @@ end
 def run
   io = IOUtils.new
   listing = mainDiff(io)
-  raise "it failed!" unless $?.exitstatus == 0
+
   listing.split("\n").each do |file|
     extension = file.split('.')[-1]
     next unless extension == 'rb'
+
     diffs = getDiffFile(io, file)
     results = analyze(diffs)
     puts results
@@ -55,5 +52,4 @@ def run
   exit 1
 end
 
-run unless ENV["CHULETAS_CLEANUP_TEST"].nil? # HACK_ME
-
+run if ENV['CHULETAS_CLEANUP_TEST'].nil? # HACK_ME
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require 'main'
 
 RSpec.describe 'main' do
   describe 'mainDiff' do
     let(:response) { "prueba.rb\nprueba3.rb\n".split("\n") }
-    let(:cmd) { 'git diff --cached --name-only --diff-filter=ACM'.split(' ') }
+    let(:cmd) { 'git diff --cached --name-only --diff-filter=ACM'.split }
     let(:io) { IOUtils.new }
     before :each do
       allow(io).to receive(:getCmdData).with(cmd).and_return(response)
